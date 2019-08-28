2caea3eaf754574829c13550df9fb5f2a30f5bf6
ss <aa>
2019-08-28 16:33:30 +0800

    sss

--- gateway/src/main/java/com/likai/gateway/filter/myFilter.java
+++ gateway/src/main/java/com/likai/gateway/filter/myFilter.java
@@ -7,20 +7,31 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Component
 @Order(3)
-public class myFilter implements WebFilterChain {
+public class myFilter implements WebFilter {
 
-    private static final Logger logger= LoggerFactory.getLogger(myFilter.class);
+    private static final Logger logger = LoggerFactory.getLogger(myFilter.class);
+
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange) {
+//        ServerHttpRequest serverHttpRequest = exchange.getRequest();
+//        ServerHttpResponse serverHttpResponse = exchange.getResponse();
+//        String ss = serverHttpRequest.getHeaders().getFirst("resign");
+//        logger.error("myFilter" + ss);
+//        return null;
+//    }
+
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange) {
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         ServerHttpRequest serverHttpRequest = exchange.getRequest();
         ServerHttpResponse serverHttpResponse = exchange.getResponse();
         String ss = serverHttpRequest.getHeaders().getFirst("resign");
-        logger.error("myFilter"+ss);
-        return null;
+        logger.error("myFilter" + ss);
+        return chain.filter(exchange);
     }
 }
